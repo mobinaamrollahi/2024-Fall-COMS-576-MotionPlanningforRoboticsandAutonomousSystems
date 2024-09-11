fe4dac61bc13bc9152228009dabaee9c20d6c0b3
mobinaamrollahi <amrollahiboyouki.mobina@gmail.com>
2024-09-10 22:26:00 -0500

    Implemented Base Classes

--- python_main.py
+++ python_main.py
@@ -1,11 +1,35 @@
 from collections import deque
 import heapq
 
+class GridEnvironment:
+    def __init__(self, X_max, Y_max, obstacles, xI, XG):
+        """
+        Initialize the environment with grid size, obstacles, initial state, and goal set.
+        
+        :param X_max: The maximum x-coordinate (exclusive).
+        :param Y_max: The maximum y-coordinate (exclusive).
+        :param obstacles: A set of coordinates representing obstacles.
+        :param xI: The initial state (x, y).
+        :param XG: A list of goal states.
+        """
+        self.X_max = X_max
+        self.Y_max = Y_max
+        self.obstacles = set(obstacles)
+        self.xI = xI
+        self.XG = XG
+        self.W = {(x, y) for x in range(self.X_max) for y in range(self.Y_max)}
+
 class StateSpace:
     # A base class to specify a state space X
+    def __init__(self, enviornment):
+        self.evn = enviornment
+        self.states = self.env.W - self.env.obstacles
+
     def __contains__(self, x) -> bool:
-    # Return whether the given state x is in the state space
-        raise NotImplementedError
+        if x not in self.states:
+            raise NotImplementedError
+        # Return whether the given state x is in the state space
+        return True
 
     def get_distance_lower_bound(self, x1, x2) -> float:
     # Return the lower bound on the distance between the given states x1 and x2
