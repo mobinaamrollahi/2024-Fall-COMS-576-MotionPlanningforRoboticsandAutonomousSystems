0991e931b26e7c1df9d4f772e00272abadee46e4
mobinaamrollahi <amrollahiboyouki.mobina@gmail.com>
2024-10-13 14:28:35 -0500

    Add abstract classes

--- HW1/python_main.py
+++ HW1/python_main.py
@@ -6,7 +6,36 @@ import math
 import numpy as np
 import json 
 
-class StateSpace: 
+class StateSpace:
+    """A base class to specify a state space X"""
+
+    def __contains__(self, x) -> bool:
+        """Return whether the given state x is in the state space"""
+        raise NotImplementedError
+    
+    def get_distance_lower_bound(self, x1, x2) -> float:
+        """Return the lower bound on the distance
+        between the given states x1 and x2
+        """
+        return 0
+
+class ActionSpace:
+    """A base class to specify an action space"""
+
+    def __call__(self, x) -> list:
+        """Return the list of all the possible actions
+        at the given state x
+        """
+        raise NotImplementedError
+    
+class StateTransition:
+    """A base class to specify a state transition function"""
+
+    def __call__(self, x, u):
+        """Return the new state obtained by applying action u at state x"""
+        raise NotImplementedError
+
+class StateSpacefor2D(StateSpace): 
     def __init__(self, X_max, Y_max, obstacles):
         """
         Initialize the environment with grid size, obstacles, initial state, and goal set.
@@ -42,7 +71,7 @@ class StateSpace:
         # Use numpy's norm function to calculate the distance
         return np.linalg.norm(x1_arr - x2_arr)
 
-class ActionSpace:
+class ActionSpacefor2D(ActionSpace):
     actions = [(0, 1), (-1, 0), (1, 0), (0, -1)]
 
     # A base class to specify an action space
@@ -61,7 +90,7 @@ class ActionSpace:
         #print("Current State", x, "Valid Actions:", valid_actions)
         return valid_actions
 
-class StateTransition:
+class StateTransitionfor2D(StateTransition):
     # A base class to specify a state transition function
     def __call__(self, x, u):
         # Return the new state obtained by applying action u at state x
@@ -172,9 +201,9 @@ if __name__ == "__main__":
     # Print the loaded data
     # print("X_max:", X_max, "Y_max:", Y_max, "obstacles:", obstacles, "Initial State:", xI, "Goal State:", XG)
 
-    X = StateSpace(X_max, Y_max, obstacles)
-    f = StateTransition()
-    U = ActionSpace(X, f)
+    X = StateSpacefor2D(X_max, Y_max, obstacles)
+    f = StateTransitionfor2D()
+    U = ActionSpacefor2D(X, f)
     
     # Run fsearch with BFS
     result_bfs = fsearch(X, U, f, xI, XG, 'bfs')
